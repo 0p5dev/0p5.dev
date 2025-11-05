95098140d1ece76107c8eec161cfc324f4bcbba9
Joshua Castellucci <joshua.castellucci@digizyne.net>
2025-11-05 16:17:38 -0500

    Dumped deployment info to screen, added charts. Need to refine the UI and make sure chart data is working and accurate.

--- app/pages/deployment/[name].vue
+++ app/pages/deployment/[name].vue
@@ -45,13 +45,36 @@
         icon="ph:share-duotone"
         :to="deployment.url"
         target="_blank"
-        class="mb-5"
       />
       <UPageFeature
         title="Container Image"
         :description="deployment.image"
         icon="ph:cube-duotone"
+        class="mt-5 mb-10"
       />
+
+      <client-only>
+        <div class="flex gap-8">
+          <UPageCard variant="subtle" class="flex-1">
+            <LineChart
+              :data="requestsData"
+              :categories="requestsCategories"
+              :height="300"
+              :xFormatter="requestsXFormatter"
+              yLabel="Requests"
+            />
+          </UPageCard>
+          <UPageCard variant="subtle" class="flex-1">
+            <LineChart
+              :data="cpuData"
+              :categories="cpuCategories"
+              :height="300"
+              :xFormatter="cpuXFormatter"
+              yLabel="CPU Usage"
+            />
+          </UPageCard>
+        </div>
+      </client-only>
     </div>
   </UContainer>
 </template>
@@ -69,6 +92,58 @@ const {
   method: "GET",
   credentials: "include",
 });
+
+import { LineChart } from "vue-chrts";
+
+const requestsCategories = {
+  requests: {
+    name: "Requests",
+    color: "#3b82f6",
+  },
+};
+
+const cpuCategories = {
+  cpu: {
+    name: "CPU Usage",
+    color: "#f97316",
+  },
+};
+
+const requestsData = computed(() => {
+  if (!deployment.value?.metrics?.requests_per_hour) {
+    return [];
+  }
+
+  return deployment.value.metrics.requests_per_hour.map(
+    (value: number, index: number) => ({
+      requests: value,
+      x: index,
+    })
+  );
+});
+
+const requestsXFormatter = (i: number) => {
+  const totalHours = requestsData.value.length - 1;
+  return `${totalHours - requestsData.value[i].x} hours ago`;
+};
+
+const cpuData = computed(() => {
+  if (!deployment.value?.metrics?.cpu_per_hour) {
+    return [];
+  }
+
+  return deployment.value.metrics.cpu_per_hour.map(
+    (value: number, index: number) => ({
+      requests: value,
+      x: index,
+    })
+  );
+});
+
+const cpuXFormatter = (i: number) => {
+  const totalHours = cpuData.value.length - 1;
+  return `${totalHours - cpuData.value[i].x} hours ago`;
+};
 </script>
 
 <style scoped></style>
